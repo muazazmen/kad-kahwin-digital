75b55c4f586c841ddd91d3db6809616e47a42bcd
themuaz97 <muaz.rahman97@gmail.com>
2024-12-17 00:05:41 +0800

    tukar theme

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -6,6 +6,7 @@ import Aura from '@primevue/themes/aura';
 import PrimeVue from 'primevue/config';
 import ConfirmationService from 'primevue/confirmationservice';
 import ToastService from 'primevue/toastservice';
+import { definePreset } from '@primevue/themes';
 
 import '@/assets/styles.scss';
 import '@/assets/tailwind.css';
@@ -13,14 +14,35 @@ import '@/assets/tailwind.css';
 const app = createApp(App);
 
 app.use(router);
+
+
+const MyPreset = definePreset(Aura, {
+    semantic: {
+        primary: {
+            50: '{cyan.50}',
+            100: '{cyan.100}',
+            200: '{cyan.200}',
+            300: '{cyan.300}',
+            400: '{cyan.400}',
+            500: '{cyan.500}',
+            600: '{cyan.600}',
+            700: '{cyan.700}',
+            800: '{cyan.800}',
+            900: '{cyan.900}',
+            950: '{cyan.950}'
+        }
+    }
+});
+
 app.use(PrimeVue, {
     theme: {
-        preset: Aura,
+        preset: MyPreset,
         options: {
             darkModeSelector: '.app-dark'
         }
     }
 });
+
 app.use(ToastService);
 app.use(ConfirmationService);
 
