a2c9cd00e706f2b4eadaeee2664d7c829e45d58f
alipacap <muhammadalifasyraf98@gmail.com>
2024-12-18 00:24:36 +0800

    modified logo.png
    changes
    - configuration.vue
    - AppMenu

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -14,6 +14,11 @@ const router = createRouter({
                     component: () => import('@/views/Dashboard.vue')
                 },
                 {
+                    path: '/configuration',
+                    name: 'Configuration',
+                    component: () => import('@/views/pages/configuration/Configuration.vue')
+                },
+                {
                     path: '/uikit/formlayout',
                     name: 'formlayout',
                     component: () => import('@/views/uikit/FormLayout.vue')
---
+++ frontend/src/views/pages/configuration/Configuration.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="card" style="text-align: center">
+        <h1>Configuration</h1>
+    </div>
+</template>
+
+<script></script>
+
+<style scoped></style>
