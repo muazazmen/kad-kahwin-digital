0abe504649a4c9335c637f46a47beb3e08759309
themuaz97 <muaz.rahman97@gmail.com>
2025-01-12 01:08:39 +0800

    initialize backend structure with TypeScript, Express, and Prisma setup

---
+++ backend/src/index.ts
@@ -0,0 +1,34 @@
+import express, { Request, Response } from 'express'
+import cors from 'cors'
+import dotenv from 'dotenv'
+import cookieParser from 'cookie-parser'
+import multer from 'multer'
+import { fileRouter } from './router/file.route.ts'
+import router from './router/index.route.ts'
+import { authRouter } from './router/auth.route.ts'
+import { adminRouter } from './router/admin.route.ts'
+
+dotenv.config()
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(cors());
+app.use(cookieParser());
+
+app.get('/', (req: Request, res: Response) => {
+  res.status(200).send({message: 'ok!'});
+})
+
+const formData = multer()
+
+// route
+app.use("/api/v1", fileRouter); // file router to stay first
+app.use("/api/v1/auth", formData.none(), authRouter);
+app.use("/api/v1/admin", formData.none(), adminRouter);
+app.use("/api/v1", formData.none(), router);
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running at port ${port}`);
+});
