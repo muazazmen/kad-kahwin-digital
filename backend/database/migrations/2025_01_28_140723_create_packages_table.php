24e146dbbda1cbc760526b5f5bfd6beceb5303cb
themuaz97 <muaz.rahman97@gmail.com>
2025-01-31 22:13:11 +0800

    Add Package model, controller, requests, policy, factory, migration, and seeder; implement basic structure for package management

---
+++ backend/database/migrations/2025_01_28_140723_create_packages_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('packages', function (Blueprint $table) {
+            $table->uuid()->primary();
+            $table->string('name');
+            $table->text('description');
+            $table->decimal('price', 10, 2);
+            $table->text('image')->nullable();
+            $table->uuid('created_by');
+            $table->uuid('updated_by')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+
+            // Define foreign key constraints explicitly
+            $table->foreign('created_by')->references('id')->on('users')->cascadeOnDelete();
+            $table->foreign('updated_by')->references('id')->on('users')->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('packages');
+    }
+};
